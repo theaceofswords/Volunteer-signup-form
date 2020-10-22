67d282be6ad8c7f98a6b6415cd8b8f440d5ee346
theaceofswords <navaneethkrishnat@gmail.com>
2020-10-22 20:16:21 +0530

    addded basic structure

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+
+const volform = document.getElementById("volform");
+var option1 =  document.getElementById("newfeild1");
+var id = document.getElementById("newfeild")
+
+
+
+function show()
+{
+	if(option1.value == "yes")
+		id.style.visibility = "visible";
+	else
+		id.style.visibility = "hidden";
+		
+}
+
+onsubmit = show();
+
+
+
+volform.addEventListener('submit',(event) =>
+{
+
+event.preventDefault();
+volform.submit();
+alert("The form was submitted \n Thank you");})
+
+
+
+
+
+ 
+
+/*onsubmit = myFunction();
+
+ function myFunction() {
+	
+	 alert("The form was submitted \n Thank you");
+  
+}*/
+
+
