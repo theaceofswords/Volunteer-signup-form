22f22c8e57a4d05ed8e52a174bd24a13cbf1cfbd
theaceofswords <navaneethkrishnat@gmail.com>
2020-10-22 20:35:35 +0530

    updated indentation

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,23 @@
 
-const volform = document.getElementById("volform");
-var option1 =  document.getElementById("newfeild1");
-var id = document.getElementById("newfeild")
 
+const volform=document.getElementById("volform");
+var option1=document.getElementById("newfeild1");
+var id=document.getElementById("newfeild");
 
+ function show() {
+     if(option1.value=="yes") id.style.visibility="visible";
+     else id.style.visibility="hidden";
 
-function show()
-{
-	if(option1.value == "yes")
-		id.style.visibility = "visible";
-	else
-		id.style.visibility = "hidden";
-		
 }
 
-onsubmit = show();
+
+onsubmit=show();
 
 
+volform.addEventListener('submit', (event)=> {
+        event.preventDefault();
+        volform.submit();
+        alert("The form was submitted \n Thank you");
+    }
 
-volform.addEventListener('submit',(event) =>
-{
-
-event.preventDefault();
-volform.submit();
-alert("The form was submitted \n Thank you");})
-
-
-
-
-
- 
-
-/*onsubmit = myFunction();
-
- function myFunction() {
-	
-	 alert("The form was submitted \n Thank you");
-  
-}*/
-
-
+)
